d852a281aefac0a272ad2ac125a781321d77d16c
RMF09 <reksamfitrananda@gmail.com>
2020-12-14 16:16:25 +0700

    spinnner input jadwal

--- app/src/main/java/com/rmf/bjbsiaga/admin/InputJadwalActivity.kt
+++ app/src/main/java/com/rmf/bjbsiaga/admin/InputJadwalActivity.kt
@@ -2,10 +2,14 @@ package com.rmf.bjbsiaga.admin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
 import com.rmf.bjbsiaga.R
 import kotlinx.android.synthetic.main.activity_input_jadwal.*
 
-class InputJadwalActivity : AppCompatActivity() {
+class InputJadwalActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+    private  val TAG = "InputJadwalActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_input_jadwal)
@@ -13,5 +17,17 @@ class InputJadwalActivity : AppCompatActivity() {
         back.setOnClickListener {
             finish()
         }
+
+        spinner_shift.onItemSelectedListener = this
+
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+        Log.d(TAG, "onItemSelected: ${parent?.getItemAtPosition(position)}")
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("Not yet implemented")
     }
 }
